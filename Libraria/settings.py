6f8b4a62694b4eb73942866038897b81407db55a
vivans <vivansvarghese0@gmail.com>
2022-08-28 21:29:48 +0530

    added static

--- Libraria/settings.py
+++ Libraria/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -155,12 +155,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS=[
+    'Libraria/static',
 ]
-MEDIA_URL='/media/'
-MEDIA_ROOT=BASE_DIR/'media/'
+
+#media configuration
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
